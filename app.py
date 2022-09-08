99ca6ef9438e79138a07f05c24af2ee9ad8de52c
Ali Naseri <alinaseri.ut@gmail.com>
2022-09-08 13:46:23 -0400

    dev-flask-app: the routes were added to a separate module

--- app.py
+++ app.py
@@ -9,28 +9,9 @@ from flask import Flask, render_template
 # instanciate the Flask class. The object name is app by convention
 app = Flask(__name__) # the variable name is passed for initialization
 
-# this points to the root folder of the server
-# multiple decorators can be used for a function
-@app.route('/')
-@app.route('/index')
-# exectue the following function
-def index():
-    # the return value of the function can be a simple string
-    # return 'Hello world, Ali is in Canada!'
-    
-    # the return value of the function can be an html string
-    # return '<h1>Welcome to my website!\n</h1><h2>my name is Ali Naseri</h2>'
-    
-    # the return value of the function can be an html template
-    # return render_template('index.html')
+# import all routes from the routes module 
+from routes import *
 
-    # the return value of the function can be an html template and an input
-    return render_template('index.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 # this command only starts the server. If you do not provide where the app should look into, it gives the "Not found" error
 # to avoid this issue, routers using decorators must be added before here
 if __name__ == '__main__':
---
+++ routes.py
@@ -0,0 +1,26 @@
+# from app module, import the app instance
+from app import app
+from flask import render_template
+
+# this points to the root folder of the server
+# multiple decorators can be used for a function
+@app.route('/')
+@app.route('/index')
+# exectue the following function
+def index():
+    # the return value of the function can be a simple string
+    # return 'Hello world, Ali is in Canada!'
+    
+    # the return value of the function can be an html string
+    # return '<h1>Welcome to my website!\n</h1><h2>my name is Ali Naseri</h2>'
+    
+    # the return value of the function can be an html template
+    # return render_template('index.html')
+
+    # the return value of the function can be an html template and an input
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
